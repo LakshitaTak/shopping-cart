ab9f4b2b3c598da02fd52375944cd8123c6f644c
Lakshita <lakshitatak33@gmail.com>
2023-11-04 22:15:12 +0530

    Updates

---
+++ precache-manifest.728f4dcf4d8575505d59017402f99f9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f94e9d9e30f6b4e2984355d990b59e9",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "d949ee5e67d529730d20",
+    "url": "/shopping-cart/static/css/main.c24f02aa.chunk.css"
+  },
+  {
+    "revision": "990ace63b2e733927bb2",
+    "url": "/shopping-cart/static/js/2.b1952a3d.chunk.js"
+  },
+  {
+    "revision": "e6ed4049a8e0fad250851e1b170b188e",
+    "url": "/shopping-cart/static/js/2.b1952a3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d949ee5e67d529730d20",
+    "url": "/shopping-cart/static/js/main.0770a9b9.chunk.js"
+  },
+  {
+    "revision": "a168b8240fa792af5021",
+    "url": "/shopping-cart/static/js/runtime-main.2f8c013a.js"
+  }
+]);
